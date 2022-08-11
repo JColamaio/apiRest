bfe3f58be0babe24ed103f1e63e3da741b49b4fb
JColamaio <jcolamaio@gmail.com>
2022-08-11 15:45:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.listen()
+app.use(express());
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Running on http://localhost:3000");
+});
